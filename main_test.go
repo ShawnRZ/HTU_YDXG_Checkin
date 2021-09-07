ac44939e084b992d35215af97fe0627b066424d3
FNDHSTD <1713252605@qq.com>
2021-09-07 12:42:01 +0800

    update

--- main_test.go
+++ main_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMain(t *testing.T) {
 	start()
 }
+
+// func TestToml(t *testing.T) {
+// 	c := Config{}
+// 	_, err := toml.DecodeFile("./configs/config.toml", &c)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	for k, v := range c.User[0].Postdata {
+// 		fmt.Printf("k: %v\n", k)
+// 		fmt.Printf("v: %v\n", v)
+// 	}
+// }
--- struct.go
+++ struct.go
@@ -8,34 +8,9 @@ type Cookie struct {
 	RememberStudentKey   string `toml:"remember_student_key"`
 	RememberStudentValue string `toml:"remember_student_value"`
 }
-type Postdata struct {
-	V string
-	Q string
-	Z string
-	X string
-	W string
-	A string
-	Y string
-	B string
-	C string
-	D string
-	E string
-	F string
-	G string
-	H string
-	I string
-	J string
-	K string
-	L string
-	M string
-	R string
-	S string
-	T string
-	U string
-}
 type User struct {
-	Cookie     Cookie   `toml:"Cookie"`
-	Postdata   Postdata `toml:"Postdata"`
+	Cookie     Cookie            `toml:"Cookie"`
+	Postdata   map[string]string `toml:"Postdata"`
 	FormID     string
 	Name       string
 	CheckinUrl string
